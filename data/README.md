65bce6250147e2228fbf8e8464f67ef25d795928
DanielDomme <dommed@oregonstate.edu>
2019-02-04 19:10:07 -0800

    I added the python script and cleaned data

--- data/README.md
+++ data/README.md
@@ -1,1 +1,3 @@
 All data collected from a varity of sources.
+The data.py file is meant to be used with python3.  It takes in the new_BMC_database_reordering.csv file and will output a Clean_BMC_DB.csv file
+The Clean_BMC_DB.csv is the desired fields that would be needed for the software.
---
+++ data/Turtle Data/data.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+import csv
+
+with open('Clean_BMC_DB.csv', 'w') as csv_file:
+    csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    line_count2 = 0
+    with open('new_BMC_database_reordering.csv', 'r') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                print('Column names are ',", ".join(row))
+                print('Count: ', len(row))
+                csv_writer.writerow([row[0],row[1],row[5],row[6],row[8],row[9],row[10],row[13],row[14],row[15],row[16],row[17],row[18],row[19],row[22],row[23],row[24]])
+                line_count += 1
+                line_count2 += 1
+            else:
+                #print('\tDate: ',row[0],', Species:',row[5],', Area: ',row[8],', State?:',row[9],', Lat:',row[11],', Long:',row[12],'.')
+                if row[8].strip() == "":
+                    row[8] = 'No Data'
+                if row[14].strip() == "":
+                    row[14] = 0
+                if row[13].strip() == "":
+                    row[13] = 0
+                if row[15].strip() == "":
+                    row[15] = 'No Data'
+                if row[16].strip() == "":
+                    row[16] = 0
+                if row[17].strip() == "":
+                    row[17] = 0
+                if row[18].strip() == "":
+                    row[18] = 0
+                if row[19].strip() == "":
+                    row[19] = 'No Data'
+                if row[20].strip() == "":
+                    row[20] = 'No Data'
+                if row[21].strip() == "":
+                    row[21] = 'No Data'
+                if row[22].strip() == "":
+                    row[22] = 'No Data'
+                csv_writer.writerow([row[0],row[1],row[5],row[6],row[8],row[9],row[10],row[13],row[14],row[15],row[16],row[17],row[18],row[19],row[22],row[23],row[25]])
+                line_count += 1
+                line_count2 += 1
+        print('Processed reading ',line_count,' lines and ',line_count2,' lines.')
