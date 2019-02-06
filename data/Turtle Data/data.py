5b5e1736c6193b4a5174022cb7d7824619838893
DanielDomme <dommed@oregonstate.edu>
2019-02-05 18:59:12 -0800

    fixed some errors and made relevant data table

--- data/Turtle Data/data.py
+++ data/Turtle Data/data.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import csv
 
-with open('Clean_BMC_DB.csv', 'w') as csv_file:
+with open('Clean_BMC_DB_ORTurt.csv', 'w') as csv_file:
     csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     line_count2 = 0
     with open('new_BMC_database_reordering.csv', 'r') as csv_file:
@@ -38,7 +38,8 @@ with open('Clean_BMC_DB.csv', 'w') as csv_file:
                     row[21] = 'No Data'
                 if row[22].strip() == "":
                     row[22] = 'No Data'
-                csv_writer.writerow([row[0],row[1],row[5],row[6],row[8],row[9],row[10],row[13],row[14],row[15],row[16],row[17],row[18],row[19],row[22],row[23],row[25]])
+                if row[10] != "California" and row[5] == 'Turtle' and row[13] != '0':
+                    csv_writer.writerow([row[0],row[1],row[5],row[6],row[8],row[9],row[10],row[13],row[14],row[15],row[16],row[17],row[18],row[19],row[22],row[23],row[24]])
                 line_count += 1
                 line_count2 += 1
         print('Processed reading ',line_count,' lines and ',line_count2,' lines.')
